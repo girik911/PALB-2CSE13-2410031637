b2e99be8af9b18cadea97e1f8e8cb9154932190e
girik911 <goswamigirik5@gmail.com>
2026-02-13 10:43:24 +0530

    Create PROBLEM7.py

---
+++ ASSIGNMENTS-HOMEWORK/ASSGNMENT2/PROBLEM7.py
@@ -0,0 +1,14 @@
+def max_subarray_sum(arr):
+    max_current = arr[0]
+    max_global = arr[0]
+    
+    for i in range(1, len(arr)):
+        max_current = max(arr[i], max_current + arr[i])
+        max_global = max(max_global, max_current)
+        
+    return max_global
+
+
+# Example
+arr = [2, 3, -8, 7, -1, 2, 3]
+print(max_subarray_sum(arr))
