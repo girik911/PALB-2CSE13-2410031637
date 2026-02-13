ef4b448418cdf3aa4a69acc19ec5930ef69cb328
girik911 <goswamigirik5@gmail.com>
2026-02-13 10:38:59 +0530

    Update problem1.py

--- ASSIGNMENTS-HOMEWORK/ASSIGNMENT1/problem1.py
+++ ASSIGNMENTS-HOMEWORK/ASSIGNMENT1/problem1.py
@@ -1,1 +1,15 @@
-print("Hello World")
+def reverse_array(arr):
+    left = 0
+    right = len(arr) - 1
+    
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+    
+    return arr
+
+
+# Example
+arr = [1, 4, 3, 2, 6, 5]
+print(reverse_array(arr))
